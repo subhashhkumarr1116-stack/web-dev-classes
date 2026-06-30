4c0ab2ef60bce943b708d4adb50287d4431328cc
subhashhkumarr1116-stack <subhashhkumarr1116@gmail.com>
2026-06-30 11:58:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+console.log("hi running");
+
+console.log(login);
+let featureX = require("./Auth.js");
+
+let {login,Auth} = require("./Auth.js")
+
+// let login = true;
+
+// console.log(login);
+// let dipesh = "bad kid";
+
+
+// function Auth(){
+//     console.log("login sucessful");
+    
+// }
+
+console.log(featureX);
+featureX.Auth();
+Auth();
