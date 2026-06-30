aad045a5e2a2284feb339bdac1307cbe003e9ca1
subhashhkumarr1116-stack <subhashhkumarr1116@gmail.com>
2026-06-30 11:58:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,16 @@
-console.log("hi running");
+// let featureX = require("./calculator.js");
+// let calculator = require("./calculator.js")
+// console.log(calculator);
 
-console.log(login);
-let featureX = require("./Auth.js");
+// let {add,sub,multi,div} = require("./calculator.js");
+// console.log(featureX);
+// add(3,2)
+// sub(9,5)
+// multi(4,6)
+// div(8,2)
 
-let {login,Auth} = require("./Auth.js")
-
-// let login = true;
-
-// console.log(login);
-// let dipesh = "bad kid";
-
-
-// function Auth(){
-//     console.log("login sucessful");
-    
-// }
-
-console.log(featureX);
-featureX.Auth();
-Auth();
+import{add,sub,multi,div} from "./calculator.js";
+console.log(add(3,4));
+console.log(sub(12,4));
+console.log(multi(3,4));
+console.log(div(16,4));
